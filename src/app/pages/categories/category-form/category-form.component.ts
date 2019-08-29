a79daf117c74fc11a5fab4d441e10a6069fb5f0f
Leonardo Sorrentino <leosorrentino@gmail.com>
2019-08-29 17:03:33 -0300

    Criando os métodos no component categoryForm

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -5,7 +5,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Category } from '../shared/category.model';
 import { CategoryService } from '../shared/category.service';
 import { switchMap } from 'rxjs/operators';
-//import toastr from toastr; 
+import toastr from 'toastr'; 
 
 @Component({
   selector: 'app-category-form',
@@ -14,12 +14,69 @@ import { switchMap } from 'rxjs/operators';
 })
 export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
-  constructor() { }
+  currentAction: string;
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMesssages: string[] = null;
+  submittingForm: boolean = false;
+  category: Category = new Category();
+
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.setCurrentAction();
+    this.buildCategoryForm();
+    this.loadCategory();
+
   }
 
   ngAfterContentChecked(){
-
+    this.setPageTitle();
   }
+
+  //Private Methods
+  private setCurrentAction(){
+    if (this.route.snapshot.url[0].path == 'new')
+      this.currentAction = 'new'
+    else
+      this.currentAction = 'edit'
+  }
+
+  private buildCategoryForm() {
+    this.categoryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null]
+    });
+  }
+
+  private loadCategory() {
+    if(this.currentAction == "edit"){
+
+      this.route.paramMap.pipe(
+        switchMap(params => this.categoryService.getById(+params.get('id')))
+      ).subscribe(
+        (category) => {
+          this.category = category;
+          this.categoryForm.patchValue(category) //binds loader category data to CategoryForm
+        },
+        (error) => alert('Ocorreu um erro no servidor, tente mais tarde')        
+      )
+    }
+  }
+
+  private setPageTitle(){
+    if(this.currentAction == 'new')
+      this.pageTitle = 'Cadastro de Nova Categoria'
+    else{
+      const categoryName = this.category.name || ""
+      this.pageTitle = 'Editando Categoria:' + categoryName;
+    }
+      
+  }
+
 }
