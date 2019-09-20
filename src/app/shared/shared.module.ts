818aa9dc9696c9c2f873df230e38ee49530151b6
Leonardo Sorrentino <leosorrentino@gmail.com>
2019-09-20 19:13:54 -0300

    Create BreadCrumbComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 
 
 @NgModule({
@@ -8,11 +9,15 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     ReactiveFormsModule
   ],  
-  declarations: [],
+  declarations: [
+    BreadCrumbComponent
+  ],
   exports: [
     //shared module
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    // shared components
+    BreadCrumbComponent
   ]
 })
 export class SharedModule { }
